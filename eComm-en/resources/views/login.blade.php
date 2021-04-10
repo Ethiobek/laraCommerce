d10b431b3a79091c37fdbd350bebe8f2a13aefc9
Bereket Zergaw <arkishasoftware@gmail.com>
2021-04-10 20:23:38 +0300

    Login complete

--- eComm-en/resources/views/login.blade.php
+++ eComm-en/resources/views/login.blade.php
@@ -3,16 +3,17 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form action="">
+            <form action="login" method="POST">
+                @csrf
                 <div class="form-group">
                     <label for="emailinput">Email address</label>
-                    <input type="email" class="form-control" placeholder="Email" id="emailinput">
+                    <input type="email" name="email" class="form-control" placeholder="Email" id="emailinput">
                 </div>
                 <div class="form-group">
                     <label for="userpassword">Password</label>
-                    <input type="password" class="form-control" placeholder="password" id="userpassword">
+                    <input type="password" name="password" class="form-control" placeholder="password" id="userpassword">
                 </div>
-                <button class="btn btn-default" type="submit">Submit</button>
+                <button class="btn btn-default" type="submit">Login</button>
             </form>
         </div>
     </div>
