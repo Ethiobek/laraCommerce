5d5bed9bfbb01cc73d43a2e55b3ba9451dc046a3
Bereket Zergaw <arkishasoftware@gmail.com>
2021-04-11 09:52:12 +0300

    Add to cart completed

--- eComm-en/app/Http/Controllers/ProductController.php
+++ eComm-en/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
@@ -39,9 +40,14 @@ class ProductController extends Controller
             return redirect('/');
         }
         else
-        {
+        { 
             return redirect('/login');
         }
         
+    }
+    static function cartItem()
+    {
+        $userId= Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
 }
